228f6eb088ef41c222050f4bd6914c8e603f6863
Murad Samadov <muradsamadov5@gmail.com>
2021-01-05 05:45:42 +0000

    initial commit

---
+++ Vagrantfile-script/script.sh
@@ -0,0 +1,9 @@
+yum update -y 
+yum install vim net-tools sshpass -y
+systemctl disable --now firewalld
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
+systemctl reload sshd.service
+echo "root" | sudo passwd --stdin root
